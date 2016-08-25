7b302314f94042865a023135478b6c50339227ea
wang_lei <wang_lei@microad-tech70.microad-tech.cn>
2016-08-25 14:42:50 +0800

    test commit after rename

--- src/EighteenChapter/ThreadTest.java
+++ src/EighteenChapter/ThreadTest.java
@@ -1,5 +1,23 @@
 package EighteenChapter;
 
-public class ThreadTest {
+public class ThreadTest extends Thread{
+    public void run() {
+        int count = 10;
+        while(true) {
+            System.out.println(count + " ");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                // TODO 自動生成された catch ブロック
+                e.printStackTrace();
+            }
+            if(--count == 0) {
+                return;
+            }
+        }
+    }
 
+    public static void main(String[] args) {
+        new ThreadTest().start();
+    }
 }
