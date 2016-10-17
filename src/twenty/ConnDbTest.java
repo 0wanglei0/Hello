fadab01c34b5424f47604f53d2d2d25df96c211f
wanglei <wang_lei@microad-tech.cn>
2016-10-17 09:14:20 +0800

    test

--- src/twenty/ConnDbTest.java
+++ src/twenty/ConnDbTest.java
@@ -2,26 +2,45 @@ package twenty;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 
 public class ConnDbTest {
+
+    private static Connection conn;
     public static void main(String[] args) {
+        getConnection();
         try {
-            Class.forName("rtb");
-            Connection conn = DriverManager.getConnection("192.168.196.14:9100", "root", "password");
-            Statement stmt = conn.createStatement();
-            String sql = "select * from dtgDb.item";
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("database driver success");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://192.168.196.14:9100", "root", "password");
+            System.out.println("database connect successed");
+            String sql = "select * from drtg_db.item";
+            PreparedStatement stmt = conn.prepareStatement(sql);
             ResultSet rs = stmt.executeQuery(sql);
             while (rs.next()) {
                 System.out.println(rs.getRow());
                 System.out.println(rs.toString());
             }
-            conn.close();
-        } catch (SQLException | ClassNotFoundException e) {
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
+        return conn;
     }
 }
